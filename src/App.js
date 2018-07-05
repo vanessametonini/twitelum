009201e426c7cfe81ebf658097897f5725066e11
Vanessa Me Tonini <vanessametonini@gmail.com>
2018-07-05 15:14:19 -0300

    Cap4. NavMenu component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component, Fragment } from 'react';
 import Cabecalho from "./components/Cabecalho";
+import NavMenu from "./components/NavMenu";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <Cabecalho usuario="@vanessametonini" />
+        <Cabecalho>
+          <NavMenu usuario="@vanessametonini" />
+        </Cabecalho>
       </Fragment>
     );
   }
