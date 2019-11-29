89e20d6914b1d924197d4b609947609df005c118
Vanessa Me Tonini <vanessametonini@gmail.com>
2019-11-29 14:58:05 -0300

    Cap14 Rotas protegidas

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,35 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+
+
+function estaAutenticado() {
+  if(localStorage.getItem('token')){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function PrivateRoute(props) {
+  const Component = props.component;
+
+  return (
+    estaAutenticado() ?
+      <Route render={() => <Component {...props} />} />
+    : 
+      <Redirect to="/login" />
+  )
+  
+}
 
 const Roteamento = () => {
   return (
     <Switch>
       <Route path="/login" component={Login} />
-      <Route path="/" component={Home} exact />
+      <PrivateRoute path="/" component={Home} exact />
     </Switch>
   )
 }
